b06d37b77c7b8f6c0d551c69a296874a3dfbaf54
Moulishwaran <moulishwaran6010@gmail.com>
2024-10-04 10:38:07 +0100

    Lists in Python

---
+++ lists.py
@@ -0,0 +1,16 @@
+#Lists with multiple values
+list1 = ['1', '2', '3']
+print('One Dimensional List',list1,)
+
+#Multi Dimensional Lists
+list2 = ['Anglia','Ruskin','University',['United Kingdom','Europe']]
+print('Multi Dimensional Lists',list2)
+
+#Accessing Element from Lists
+print('First Element in list1:',list1[0])
+print('First Element in list2:',list2[0])
+
+#Accessing using negative indexing
+print('Last Element in list1:',list1[-1])
+print('Last Element in list2:',list2[-1])
+print('Last Before Element in list2:',list2[-2])
