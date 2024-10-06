cd8cdb2085fe4e259b4eb0359ecf5378cb54b781
Moulishwaran <moulishwaran6010@gmail.com>
2024-10-07 00:11:55 +0100

    tuples practice

---
+++ modules.py
@@ -0,0 +1,5 @@
+# 1.Module: A Python file containing functions, classes, or variables that you can import into other files.
+# 2.Import: Use the import keyword to bring a module's functionality into your code.
+# 3.Standard Library: Python comes with many built-in modules, like math, random, and os.
+# 4.Third-party Modules: You can install external modules using pip.
+# 5.Package: A way to organize related modules into directories.
---
+++ tuples.py
@@ -0,0 +1,9 @@
+#creating a tuple
+tuple1 = ('Tuples','are','immutable')
+print (tuple1)
+
+#creating a tuple using a list
+tuple_list1 = ['1','2','3','4']
+tuple1 = tuple(tuple_list1)
+print(tuple1)
+
